c153127b0ecb1c0449afc36af4d8a5b2fb0875b9
Kenshin-Shibata <ps0139hk@gmail.com>
2022-01-27 21:19:56 +0900

    #5 Reusable todo

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,12 +4,13 @@ import "./styles.css"
 interface Props{
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: () => void;
 }
 
 
-const InputField = ({todo, setTodo}: Props) => {
+const InputField = ({todo, setTodo, handleAdd }: Props) => {
   return (
-    <form className='input'>
+    <form className='input' onSubmit={handleAdd}>
         <input 
             type="input" 
             value={todo}
