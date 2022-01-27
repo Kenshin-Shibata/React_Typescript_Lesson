bac08bb772ebbec658954348ddcd185595f3ff43
Kenshin-Shibata <ps0139hk@gmail.com>
2022-01-27 20:59:13 +0900

    #3 useState Hook with TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import InputField from "./components/InputField"
+import InputField from "./components/InputField";
 
-const App: React.FC= () => {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  console.log(todo)
+
   return (
     <div className="App">
-      <span className="heading">Taskfy</span>
-      <InputField />
-
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
-}
+};
 
 export default App;
